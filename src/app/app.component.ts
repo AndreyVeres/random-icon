0d01fb709a4d540339e7c555cbd0d75b376373ab
Andrey <spacecmetanka@gmail.com>
2023-04-12 14:08:48 +0300

    feat: show random icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import * as icons from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'random-icon';
+  private AllIcons
+  currentIcon
+  constructor() {
+    this.AllIcons = Array.from(Object.entries({ ...icons }))
+    this.currentIcon = ''
+  }
+
+  randomInteger() {
+    let rand = 0 - 0.5 + Math.random() * (this.AllIcons.length - 0 + 1);
+    return Math.round(rand);
+  }
+
+  show = () => {
+    const { prefix, iconName } = this.AllIcons[this.randomInteger()][1] as { prefix: string, iconName: string }
+
+    const timeOut = setTimeout(() => {
+      this.currentIcon = `${prefix} fa-${iconName}`
+      clearTimeout(timeOut)
+    }, 3000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
